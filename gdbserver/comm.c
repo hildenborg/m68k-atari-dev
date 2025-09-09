dc65e23c49f2d4edd0420588c57649838609099e
Mikael Hildenborg <micael@hildenborg.com>
2025-09-09 20:04:29 +0200

    Falcon serial wip

--- gdbserver/comm.c
+++ gdbserver/comm.c
@@ -10,10 +10,15 @@
 extern short StringCompare(const char* str_a, const char* str_b);
 
 volatile short CtrlC_enable;
-volatile unsigned char Mfp_ActiveEdgeRegister __attribute__((aligned(2))) = 0;
+volatile unsigned char Mfp_ActiveEdgeRegister;
+volatile unsigned char Scc_StatusRegister;
 
 void InitMfpCom1(_CommException CommException);
 void ExitMfpCom1(void);
+
+void InitSccCom1(_CommException CommException);
+void ExitSccCom1(void);
+
 
 bool Mfp_IsMyDevice(char *comString)
 {
@@ -84,7 +89,7 @@ int Mfp_ReceiveByte(void)
 }
 
 
-void Mfp_EnableCtrlC(bool enable)
+void Mfp_Scc_EnableCtrlC(bool enable)
 {
 	CtrlC_enable = enable ? 1 : 0;
 }
@@ -98,18 +103,81 @@ void CreateMfpSerial(comm* com)
 	com->TransmitByte = Mfp_TransmitByte;
 	com->ReceiveByte = Mfp_ReceiveByte;
 	com->IsConnected = Mfp_IsConnected;
-	com->EnableCtrlC = Mfp_EnableCtrlC;
+	com->EnableCtrlC = Mfp_Scc_EnableCtrlC;
+}
+
+bool Scc_IsMyDevice(char *comString)
+{
+	return (StringCompare("COM1", comString) >= 0);
+}
+
+int Scc_Init(char *comString, _CommException CommException)
+{
+	InitSccCom1(CommException);
+
+	// Set serial conf
+//	Rsconf(BAUD_9600, FLOW_HARD, RS_CLK16 | RS_1STOP | RS_8BITS, RS_INQUIRE, RS_INQUIRE, RS_INQUIRE);
+	// Empty serial buffer
+	while (Bconstat(DEV_AUX) != 0)
+	{
+		Bconin(DEV_AUX);
+	}
+	Scc_StatusRegister = 0xff;
+
+//	Ongibit(GI_DTR);
+	return 0;
+}
+
+void Scc_Exit(void)
+{
+	
+	ExitSccCom1();
+}
+
+
+bool Scc_IsConnected(void)
+{
+	return (Scc_StatusRegister & 0x08) != 0;
+}
+
+
+int Scc_TransmitByte(unsigned char byte)
+{
+	if (Bcostat(DEV_AUX) == 0)
+	{
+		if (!Scc_IsConnected())
+		{
+			return COMM_ERR_DISCONNECTED;
+		}
+		return COMM_ERR_NOT_READY;
+	}
+	Bconout(DEV_AUX, (unsigned short)byte);
+	return 0;
+}
+
+
+int Scc_ReceiveByte(void)
+{
+	if (Bconstat(DEV_AUX) == 0)
+	{
+		if (!Scc_IsConnected())
+		{
+			return COMM_ERR_DISCONNECTED;
+		}
+		return COMM_ERR_NOT_READY;
+	}
+	return Bconin(DEV_AUX) & 0xff;
 }
 
 void CreateSccSerial(comm* com)
 {
-	com->IsMyDevice = Mfp_IsMyDevice;
-	com->Init = Mfp_Init;
-	com->Exit = Mfp_Exit;
-	com->TransmitByte = Mfp_TransmitByte;
-	com->ReceiveByte = Mfp_ReceiveByte;
-	com->IsConnected = Mfp_IsConnected;
-	com->EnableCtrlC = Mfp_EnableCtrlC;
+	com->IsMyDevice = Scc_IsMyDevice;
+	com->Init = Scc_Init;
+	com->Exit = Scc_Exit;
+	com->TransmitByte = Scc_TransmitByte;
+	com->ReceiveByte = Scc_ReceiveByte;
+	com->IsConnected = Scc_IsConnected;
+	com->EnableCtrlC = Mfp_Scc_EnableCtrlC;
 }
 
 int InitComm(const char *comString, comm* com)
--- gdbserver/comm_mfp_scc.s
+++ gdbserver/comm_mfp_scc.s
@@ -11,15 +11,15 @@
 	.global InitMfpCom1
 InitMfpCom1:
 	.func InitMfpCom1
-	move.l	4(a7), SerialExceptionCall + 2
+	move.l	4(a7), MfpSerialExceptionCall + 2
 
 	move.w	sr, -(a7)
 	ori.w	#0x700, sr
 	move.b	0xfffffa03.w, Mfp_ActiveEdgeRegister
 	move.l	0x104.w, oMfpDcd + 2
 	move.l	#MfpDcd, 0x104.w
-	move.l	0x130.w, oSerialInput + 2
-	move.l	#SerialInput, 0x130.w
+	move.l	0x130.w, oMfpSerialInput + 2
+	move.l	#MfpSerialInput, 0x130.w
 	move.w	(a7)+, sr
 	rts
 	.endfunc
@@ -30,38 +30,98 @@ ExitMfpCom1:
 	move.w	sr, -(a7)
 	ori.w	#0x700, sr
 	move.l	oMfpDcd + 2, 0x104.w
-	move.l	oSerialInput + 2, 0x130.w
+	move.l	oMfpSerialInput + 2, 0x130.w
 	move.w	(a7)+, sr
 	rts
 	.endfunc
 
-/*
-	Special for serial comm.
-	If CTRL-C checking is enabled, and a CTRL-C (0x03) is detected, then pause the execution.
-*/
-SerialInput:
+MfpSerialInput:
 	tst.w	CtrlC_enable
-	beq.s	oSerialInput
+	beq.s	oMfpSerialInput
 	btst	#7, 0xfffffa2b.w
-	beq.s	o2SerialInput
+	beq.s	o2MfpSerialInput
 	cmp.b	#3, 0xfffffa2f.w	| CTRL-C from gdb
-	bne.s	o2SerialInput
+	bne.s	o2MfpSerialInput
 	ori.w	#0x700, sr
 	clr.w	CtrlC_enable	| Clear CtrlC_enable so we don't handle that while the server context is running. 
 	move.b	#0xef, 0xfffffa0f.w	| enable irq again so serial comm works in server.
-SerialExceptionCall:
+MfpSerialExceptionCall:
 	jmp		0x12345678
-o2SerialInput:
+o2MfpSerialInput:
 	move.b	#0xef, 0xfffffa0f.w
 	rte
-oSerialInput:
+oMfpSerialInput:
 	jmp 	0x12345678
 
-/*
-	Capture the mfp state, so we can easily detect DCD
-*/
 MfpDcd:
 	move.b	0xfffffa03.w, Mfp_ActiveEdgeRegister
 oMfpDcd:
+	jmp 	0x12345678
+
+	.global InitSccCom1
+InitSccCom1:
+	.func InitSccCom1
+	move.l	4(a7), SccSerialExceptionCall + 2
+	move.w	sr, -(a7)
+	ori.w	#0x700, sr
+
+/*
+	move.l	0x188.w, oSccStatus + 2
+	move.l	#SccStatus, 0x188.w
+	move.l	0x190.w, oSccSerialInput + 2
+	move.l	#SccSerialInput, 0x190.w
+*/
+|	move.b	#0, 0xffff8c85.w
+|	move.b	0xffff8c85.w, Scc_StatusRegister
+|	move.b	#5, 0xffff8c85.w
+|	move.b	#0xea, 0xffff8c85.w
+	move.w	(a7)+, sr
+	moveq	#0, d0
+	rts
+	.endfunc
+
+	.global ExitSccCom1
+ExitSccCom1:
+	.func ExitSccCom1
+	move.w	sr, -(a7)
+	ori.w	#0x700, sr
+
+/*
+	move.l	oSccStatus + 2, 0x188.w
+	move.l	oSccSerialInput + 2, 0x190.w
+*/
+	move.w	(a7)+, sr
+	rts
+	.endfunc
+
+SccStatus:
+	move.b	#0, 0xffff8c85.w
+	move.b	0xffff8c85.w, Scc_StatusRegister
+oSccStatus:
+	jmp 	0x12345678
+
+SccSerialInput:
+	tst.w	CtrlC_enable
+	jeq		oSccSerialInput
+	move.b	#0, 0xffff8c85.w
+	btst	#0, 0xffff8c85.w
+	jeq		o2SccSerialInput
+	cmp.b	#3, 0xffff8c87.w	| CTRL-C from gdb
+	jne		o2SccSerialInput
+	ori.w	#0x700, sr
+	clr.w	CtrlC_enable	| Clear CtrlC_enable so we don't handle that while the server context is running. 
+	move.b	#0, 0xffff8c85.w
+	move.b	#0x30, 0xffff8c85.w		|reset ext/status int
+	move.b	#0, 0xffff8c85.w
+	move.b	#0x38, 0xffff8c85.w		|reset highest IUS
+SccSerialExceptionCall:
+	jmp		0x12345678
+o2SccSerialInput:
+	move.b	#0, 0xffff8c85.w
+	move.b	#0x30, 0xffff8c85.w		|reset ext/status int
+	move.b	#0, 0xffff8c85.w
+	move.b	#0x38, 0xffff8c85.w		|reset highest IUS
+	rte
+oSccSerialInput:
 	jmp 	0x12345678
 
